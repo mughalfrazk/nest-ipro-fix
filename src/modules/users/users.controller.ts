1fc4791a7aa815e2934d5ecbb0852174238d95ad
IproFixGH <crmt5169@gmail.com>
2024-10-30 14:57:40 +0000

    chore: no. of jobs in users api

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,5 +1,5 @@
-import { BadRequestException, Controller, Get, Query } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { BadRequestException, Controller, Get, Param, Query } from "@nestjs/common";
+import { ApiParam, ApiTags } from "@nestjs/swagger";
 import { UsersService } from "./users.service";
 import { AuthUser } from "@/decorators/auth-user.decorator";
 import { Roles } from "@/auth/decorators/roles.decorator";
@@ -17,7 +17,8 @@ export class UsersController {
   @Get()
   @Roles(["super_admin", "admin"])
   async getByRole(@Query("role_id") role_id: string, @Query("speciality_id") speciality_id: string, @AuthUser() { company }) {
-    return this.usersService.findByRoleAndSpeciality(company.id, role_id, speciality_id)
+    const users = await this.usersService.findByRoleAndSpeciality(company.id, role_id, speciality_id)
+    return users.map(user => ({ ...user, jobs: user.jobs.reduce(prev => prev + 1, 0) }))
   }
 
   @Get("/technician")
@@ -29,5 +30,16 @@ export class UsersController {
     }
 
     return this.usersService.findByRoleAndSpeciality(company.id, role.id, speciality_id)
+  }
+
+  @Get(":id")
+  @ApiParam({ name: "id", required: true })
+  @Roles(["admin", "receptionist", "staff"])
+  async getUserById(@Param("id") id: string) {
+    const user = await this.usersService.findById(id)
+
+    if (!user) throw new BadRequestException("User not found.")
+
+    return user
   }
 }
