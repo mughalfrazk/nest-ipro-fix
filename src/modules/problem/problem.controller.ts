a1086b67d9d1d05690635ba8410e396e541f27c9
Faraz Mughal <mughalfrazk@gmail.com>
2024-11-21 22:03:05 +0000

    feat: brand, models, problem crud

--- src/modules/problem/problem.controller.ts
+++ src/modules/problem/problem.controller.ts
@@ -1,7 +1,8 @@
-import { BadRequestException, Body, Controller, Get, Post } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { ProblemService } from "./problem.service";
 import { CreateProblem } from "./dto/create-problem.dto";
+import { UpdateProblem } from "./dto/update-problem.dto";
 
 @ApiTags("Problem")
 @Controller("problem")
@@ -19,5 +20,18 @@ export class ProblemController {
     if (brandEntity) throw new BadRequestException("Problem already exists.")
 
     return this.problemService.create(body)
+  }
+
+  @Patch(':id')
+  async update(@Param("id") id: number, @Body() body: UpdateProblem) {
+    const problemEntity = await this.problemService.findById(id);
+    if (!problemEntity) throw new BadRequestException("Issue name not found.")
+
+    return this.problemService.update(id, body)
+  }
+
+  @Delete(':id')
+  async delete(@Param("id") id: string) {
+    await this.problemService.deleteRow(id)
   }
 }
--- src/modules/problem/problem.service.ts
+++ src/modules/problem/problem.service.ts
@@ -1,27 +1,40 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Problem } from "./problem.entity";
-import { Repository } from "typeorm";
+import { IsNull, Repository } from "typeorm";
 import { CreateProblem } from "./dto/create-problem.dto";
 import { problemSeeder } from "./problem.seeder";
+import { UpdateProblem } from "./dto/update-problem.dto";
 
 @Injectable()
 export class ProblemService {
   constructor(@InjectRepository(Problem) private repo: Repository<Problem>) { }
 
   async findAll() {
-    return this.repo.find()
+    return this.repo.find({ where: { deleted_at: IsNull() } })
+  }
+
+  async findById(id: number) {
+    return this.repo.findOne({ where: { id, deleted_at: IsNull() } })
   }
 
   async findByName(name: string) {
-    return this.repo.findOne({ where: { name } })
+    return this.repo.findOne({ where: { name, deleted_at: IsNull() } })
   }
-
   async create(payload: CreateProblem) {
     const { name, description } = payload;
 
     const entity = this.repo.create({ name, description })
     return this.repo.save(entity)
+  }
+
+  async update(id: number, payload: UpdateProblem) {
+    await this.repo.update(id, payload)
+    return this.findById(id)
+  }
+
+  async deleteRow(id: string) {
+    return this.repo.update(id, { deleted_at: new Date() })
   }
 
   async seed() {
